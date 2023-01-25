ea5e141fa301645461ffa62ddf804d34276271e8
momemtwithace <augustineezekiel763@gmail.com>
2023-01-25 13:20:48 +0100

    Built Weather Forecast Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:weather_forecast/screens/weather_forecast_ui_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,12 +10,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
+        
                 primarySwatch: Colors.blue,
       ),
       home: WeatherForecastUIScreen(),
